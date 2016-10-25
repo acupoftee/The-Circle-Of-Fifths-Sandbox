a39c32388aec7c6143157bcb9d0a2082ff9d0f68
BethyDiakabananas <bethyd123@gmail.com>
2016-10-25 17:47:02 -0400

    adding colors.js and notes.js as well as updated helper files

---
+++ js/wheel/Notes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by diakabanab on 10/23/2016.
+ */
+define(["teoria", "chord/Positions"], function(teoria, Positions) {
+    var notes = {
+        major: {},
+        minor: {}
+    };
+});
