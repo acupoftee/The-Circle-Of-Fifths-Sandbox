209d0632f57af7f283448c879c7e1bed8f90d5fe
BethyDiakabananas <bethyd123@gmail.com>
2016-10-25 17:33:23 -0400

    drawing major arc in Positions.js

--- js/chord/Positions.js
+++ js/chord/Positions.js
@@ -18,6 +18,29 @@ define(function() {
     var innerRadius = 0.66;
 
     var majorOrder = ["C", "G", "D", "A", "E", "B", "F#", "C#", "G#", "A#", "F"];
+    var minorOrder = ["A", "E", "B", "F#", "C#", "G#", "D#", "A#", "F", "C", "G", "D"];
+    var majorCoordinates = {};
+    var minorCoordinates = {};
+
+    var threshold = 0.01;
+
+    // draws major slices first
+    var arcSize = Math.PI * 2 / majorOrder.length;
+    var startAngle = 0 - arcSize / 2 - Math.PI / 2;
+
+    for (var i = 0; i < majorOrder.length; i++) {
+        var coordinates = {
+            startAngle : startAngle - threshold,
+            endAngle : startAngle + arcSize + threshold,
+            innerRadius : innerRadius,
+            outerRadius : 1,
+        };
+        coordinates.center = toCartesian((coordinates.endAngle + coordinates.startAngle) / 2,
+            (coordinates.outerRadius + coordinates.innerRadius) / 2);
+        majorCoordinates[majorOrder[i]] = coordinates;
+        startAngle += arcSize;
+    }
+
 
 
 })
