39fffc20357a2e723bc6cdc62acd17a016924b4a
BethyDiakabananas <bethyd123@gmail.com>
2016-10-26 17:42:49 -0400

    adding finished notes.js

--- js/wheel/Notes.js
+++ js/wheel/Notes.js
@@ -6,4 +6,26 @@ define(["teoria", "chord/Positions"], function(teoria, Positions) {
         major: {},
         minor: {}
     };
+
+    // initializes major and minor notes
+    for (var i = 0; i < Positions.majorOrder.length; i++) {
+        var key = Positions.majorOrder[i];
+        var major = teoria.note(key + "3").chord("major");
+        var minor = teoria.note(key + "3").chord("minor");
+        notes.major[key] = [];
+        notes.minor[key] = [];
+        var octaves = 3;
+        for (var o = 0; o < octaves; o++) {
+            var majorNotes = major.notes();
+            var minorNotes = minor.notes();
+            for (var j = 0; j < majorNotes.length; j++) {
+                notes.major[key].push(majorNotes[j].midi());
+                notes.minor[key].push(minorNotes[j].midi());
+            }
+            major.transpose(teoria.interval("P8"));
+            minor.transpose(teoria.interval("P8"));
+        }
+    }
+
+    return notes;
 });
