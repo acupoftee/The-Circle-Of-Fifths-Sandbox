95f07e40de8db49d0d3faa000b777e9be72dfee6
BethyDiakabananas <bethyd123@gmail.com>
2016-10-28 09:29:56 -0400

    adding the prototype getChord in interface.js

--- js/chord/Interface.js
+++ js/chord/Interface.js
@@ -7,8 +7,41 @@ define(["jquery", "chord/Positions"], function($, Positions) {
             "id": "Interaction"
         }).appendTo(container);
 
-        // this.element.on("mousedown", this.mousedown.bind(this));
-        // this.element.on("mousedown", this.mousedown.bind(this));
+        this.element.on("mousedown", this.mouseup.bind(this));
+        this.element.on("touchstart", this.click.bind(this));
+        this.element.on("touchend", this.mouseup.bind(this));
+        this.element.on("mouseup", this.mouseup.bind(this));
 
-    }
+        this.onstart = function(){};
+        this.onend = function(){};
+    };
+
+    /**
+     * Gets the chord at its given position
+     */
+    Interface.prototype.getChord = function(x, y) {
+        var twoPi = Math.PI * 2;
+        var width = this.element.width;
+        var height = this.element.height;
+        x = ((x - width / 2) / width) * 2;
+        y = ((y - height / 2) / height) * 2;
+        var theta = Math.atan2(y, x);
+        var radius = Math.sqrt(x*x + y*y);
+
+        var i = 0;
+        for (var letter in Positions.major) {
+            var coordinate = Positions.major[letter];
+            if (theta > coordinate.startAngle && theta < coordinate.endAngle) {
+                break;
+            } else if (theta + twoPu > coordinate.startAngle && theta + twoPi < coordinate.endAngle) {
+                break;
+            }
+            i++;
+        }
+        if (radius < Positions.innerRadius) {
+            return [Positions.minorOrder[i], "minor"];
+        } else {
+            return [Positions.majorOrder[i], "major"];
+        }
+    };
 });
