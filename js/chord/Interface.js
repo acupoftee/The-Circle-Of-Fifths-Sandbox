b205063cbe78c86abe081f7552821af3f8ee203b
BethyDiakabananas <bethyd123@gmail.com>
2016-10-27 23:15:26 -0400

    adding interface.js

---
+++ js/chord/Interface.js
@@ -0,0 +1,14 @@
+/**
+ * Created by diakabanab on 10/26/2016.
+ */
+define(["jquery", "chord/Positions"], function($, Positions) {
+    var Interface = function(container) {
+        this.element = $("<div>", {
+            "id": "Interaction"
+        }).appendTo(container);
+
+        // this.element.on("mousedown", this.mousedown.bind(this));
+        // this.element.on("mousedown", this.mousedown.bind(this));
+
+    }
+});
